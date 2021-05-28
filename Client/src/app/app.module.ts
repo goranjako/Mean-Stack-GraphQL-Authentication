c3bd1728aa7c67425c16315011af55f7fb42ac23
Goran Jakovljević <goranjako@gmail.com>
2021-05-28 21:54:46 +0200

    apollo

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,  MDBBootstrapModule.forRoot()
+    AppRoutingModule,  MDBBootstrapModule.forRoot(), GraphQLModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
